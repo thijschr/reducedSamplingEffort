7a387012c68defb376078f6309dba95aafc68390
TCvSon <thijs.vanson@runbox.com>
2013-12-06 13:56:34 +0100

    Added section for intra-site variability analysis

--- R/analysisSamlingEffort.R
+++ R/analysisSamlingEffort.R
@@ -6,3 +6,47 @@
 allStaRep <- read.csv("data/abundanceByStationAndSample.csv")
 allSta <- read.csv("data/abundanceByStation.csv")
 
+
+
+
+# ANALYSIS OF INTRA-SITE VARIABILITY --------------------------------------
+
+# ORDINATION OF FULL DATASET BY GRAB --------------------------------------
+
+# decorana() call and extraction of axes scores ---------------------------
+require(vegan)
+
+dca4StaRep <- decorana(allStaRep16[, -c(1:2)])
+
+axes4StaRep <- data.frame(dca1 = scores(dca4StaRep, choices = 1, display = "sites"),
+                          dca2 = scores(dca4StaRep, choices = 2, display = "sites"))
+
+## Adding Site/Station as grouping factor
+axes4StaRep$Sta <- allStaRep16$Sta
+
+
+# Extracting mean and range of DCA site-grab scores -----------------------
+
+axes4StaRepMeanRange <- data.frame(
+    d1 = aggregate(x = axes4StaRep$dca1,
+                   by = list(axes4StaRep$Sta),
+                   FUN = mean)[, 2],
+    d1Lo = aggregate(x = axes4StaRep$dca1,
+                     by = list(axes4StaRep$Sta),
+                     FUN = range)[, 2][, 1],
+    d1Hi = aggregate(x = axes4StaRep$dca1,
+                     by = list(axes4StaRep$Sta),
+                     FUN = range)[, 2][, 2],
+    d2 = aggregate(x = axes4StaRep$dca2,
+                   by = list(axes4StaRep$Sta),
+                   FUN = mean)[, 2],
+    d2Lo = aggregate(x = axes4StaRep$dca2,
+                     by = list(axes4StaRep$Sta),
+                     FUN = range)[, 2][, 1],
+    d2Hi = aggregate(x = axes4StaRep$dca2,
+                     by = list(axes4StaRep$Sta),
+                     FUN = range)[, 2][, 2])
+
+axes4StaRepMeanRange$deltaD1 <- with(axes4StaRepMeanRange, d1Hi - d1Lo)
+axes4StaRepMeanRange$deltaD2 <- with(axes4StaRepMeanRange, d2Hi - d2Lo)
+
