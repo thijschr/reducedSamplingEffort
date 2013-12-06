9deb36a45759406bb50b8e45e0649dba2ffabe9e
TCvSon <thijs.vanson@runbox.com>
2013-12-06 12:57:00 +0100

    Added a function that randomly selects samples within a site

--- R/functionsSamplingEffort.R
+++ R/functionsSamplingEffort.R
@@ -1,5 +1,12 @@
 ### Source file with functions
 # Source it in R: source(functionsSamplingEffort.R)
 
+# Function for selecting random rows/samples within a site ---------------
 
+randomRows <- function(data, n) 
+    ## data: dataframe with site (1st col), sample (2nd), and species abundances
+    ## n: number of samples to sample within a site
+{
+    return(data[sample(nrow(data), n), ])
+}
 
