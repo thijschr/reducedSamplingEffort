0d92e83828ba1969a4f5b3698c9c1dd188014cff
TCvSon <thijs.vanson@runbox.com>
2013-12-06 13:34:00 +0100

    Added section for loading data

--- R/analysisSamlingEffort.R
+++ R/analysisSamlingEffort.R
@@ -1,2 +1,8 @@
 ### Analysis of sampling effort
 
+
+# Loading data ------------------------------------------------------------
+
+allStaRep <- read.csv("data/abundanceByStationAndSample.csv")
+allSta <- read.csv("data/abundanceByStation.csv")
+
