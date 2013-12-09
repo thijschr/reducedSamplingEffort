a32afc236350f3942838aac9b674da06581b6386
TCvSon <thijs.vanson@runbox.com>
2013-12-09 14:57:35 +0100

    Extracted the percIndices function from the procrPlots function

--- R/functionsSamplingEffort.R
+++ R/functionsSamplingEffort.R
@@ -102,62 +102,78 @@ signCheck <- function(source, target) {
 
 # Function conducting procrustes analysis ---------------------------------
 
-procrAnalysis <- function(source, first, second) {
-    ## source: data frame/matrix containing ordination axes of the full data set
+procrAnalysis <- function(target, first, second) {
+    ## target: data frame/matrix containing ordination axes of the full data set
     ## first: data frame/matrix containing first axes of reduced data sets
     ## second: data frame/matrix containing second axes of reduced data sets
-    # coeffs <- rep(NA, ncol(first))
+    
     procrRes <- list()
     
     for(i in 1:ncol(first)) {
-        target.i <- cbind(first[, i], second[, i])
-        procr.i <- protest(X = source, Y = target.i,
+        rotated.i <- cbind(first[, i], second[, i])
+        procr.i <- protest(X = target, Y = rotated.i,
                            scores = "sites",
                            symmetric = T,
                            permutations = 999)
-        # coeffs[i] <- procr.i$scale
         procrRes[[length(procrRes) + 1]] <- procr.i
     }
     procrRes
 }
 
 
+
+# Function for selecting indices matching percentiles ---------------------
+
+percIndices <- function(procr) {
+    ### Selects iteration indices based on 0, 20, 40, 60, 80, and 100% percentiles
+    ## procr: output from a protest() call
+    index <- rep(NA, 6)
+    coeffs <- unlist(lapply(procr, function(x) x$scale))
+    perc <- unname(round(quantile(coeffs, probs = seq(0, 1, 0.2)), 2))
+    for(i in seq_along(perc)) {
+        index[i] <- match(perc[i], round(coeffs, 2))
+    }
+    index
+}
+
+
 # Function selecting procrustes coefficients and then plotting ------------
 
-# procrPlots <- function(coeffs) {
-#     ### Makes a 3x2 plot showing procrustes plots for the 0, 20, 40, 60, 80, and 100 
-#     ### percentiles of procrustes coefficients
-#     ## coeffs: vector of procrustes coefficients
-#     ## 
-#     index <- rep(NA, 6)
-#     perc <- unname(round(quantile(coeffs, probs = seq(0, 1, 0.2)), 2))
-#     for(i in seq_len(perc)) {
-#         index[i] <- match(perc[i], round(coeffs, 2))
-#     }
-#     
-#     pdf(file = paste("output/", "procr41Plots.pdf", sep = ""),
-#         height = 10, width = 10)
-#     
-# #     svg(file = paste(avhandling, "fig2_procr41Plots.svg", sep = ""),
-# #         height = 10, width = 10,
-# #         onefile = TRUE)
-#     
-#     par(mfrow = c(3, 2))
-#     
-#     for(i in index)
-#     {
-#         plot(procr41Ls[[i]],
-#              xlab = "DCA1", ylab = "DCA2", 
-#              main = paste("r = ", round(procr41[i], 2), sep = ""),
-#              cex.main = 1.5,
-#              xlim = c(-0.3, 0.6), ylim = c(-0.25, 0.25))
-#         text(procr41Ls[[i]],
-#              display = "rotated",
-#              labels = 1:28,
-#              cex = 1.25,
-#              pos = 2,
-#              offset = 0.25)
-#     }
-#     dev.off()
-# }
-# 
+procrPlots <- function(procr, index, ...) {
+    ### Makes a 3x2 plot showing procrustes plots for the 0, 20, 40, 60, 80, and 100 
+    ### percentiles of procrustes coefficients
+    ## procr: output from a protest() call
+    ## index: vector of indices returned from percIndices() call
+    
+    procrSel <- procr[index]
+    percLabels <- seq(0, 100, 20)
+    coeffs <- unlist(lapply(procrSel, function(x) x$scale))
+       
+    pdf(file = paste("output/", "procr41Plots.pdf", sep = ""),
+        height = 9, width = 7)
+    
+#     svg(file = paste(avhandling, "fig2_procr41Plots.svg", sep = ""),
+#         height = 10, width = 10,
+#         onefile = TRUE)
+    
+    par(mfrow = c(3, 2))
+    
+    for(i in 1:length(procrSel))
+    {
+        plot(procrSel[[i]],
+             xlab = "DCA1", ylab = "DCA2",
+             to.target = F,
+             main = paste("r =", round(coeffs[i], 2), ",",
+                          percLabels[i], "% Percentile", 
+                          sep = " "),
+             cex.main = 1.5,
+             xlim = c(-0.3, 0.6), ylim = c(-0.25, 0.25))
+        text(procrSel[[i]],
+             display = "rotated",
+             labels = 1:28,
+             cex = 1.25,
+             pos = 2,
+             offset = 0.25)
+    }
+    dev.off()
+}  
